e67a62a3ddf1fe65bcde7e337ac05f910adf6120
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-08-07 01:34:23 -0400

    added the map

---
+++ assets/script3.js
@@ -0,0 +1,60 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: { lat: 95.7129, lng: 37.0902  }, // Australia.
+    });
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer({
+      draggable: true,
+      map,
+      panel: document.getElementById("panel"),
+    });
+    directionsRenderer.addListener("directions_changed", () => {
+      const directions = directionsRenderer.getDirections();
+  
+      if (directions) {
+        computeTotalDistance(directions);
+      }
+    });
+    displayRoute(
+      "New York, NY",
+      "Los Angeles, CA",
+      directionsService,
+      directionsRenderer
+    );
+  }
+  
+  function displayRoute(origin, destination, service, display) {
+    service
+      .route({
+        origin: origin,
+        destination: destination,
+        waypoints: [
+          { location: "Orlando, FL" },
+          { location: "Denver, CO" },
+        ],
+        travelMode: google.maps.TravelMode.DRIVING,
+        avoidTolls: true,
+      })
+      .then((result) => {
+        display.setDirections(result);
+      })
+      .catch((e) => {
+        alert("Could not display directions due to: " + e);
+      });
+  }
+  
+  function computeTotalDistance(result) {
+    let total = 0;
+    const myroute = result.routes[0];
+  
+    if (!myroute) {
+      return;
+    }
+  
+    for (let i = 0; i < myroute.legs.length; i++) {
+      total += myroute.legs[i].distance.value;
+    }
+    total = total / 1000;
+    document.getElementById("total").innerHTML = total + " km";
+  }
